af77909434be30e46962d2dd9b0e7b0b819f4c88
niranjanmaradapu <85561667+niranjanmaradapu@users.noreply.github.com>
2022-12-07 16:13:10 +0530

    Add files via upload
    Initial commit

---
+++ test-demo/src/main/java/com/otsi/testdemo/dto/TestVO.java
@@ -0,0 +1,15 @@
+package com.otsi.testdemo.dto;
+
+public class TestVO {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
